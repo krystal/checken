a9c907b0bbc068959ead7409918787768029a9a3
Paul Sturgess <paul.sturgess@krystal.uk>
2025-03-27 17:42:43 +0000

    feat: allow optional namespace configuration

--- lib/checken/config.rb
+++ lib/checken/config.rb
@@ -4,6 +4,30 @@ require 'checken/user_proxy'
 module Checken
   class Config
 
+    DEFAULT_NAMESPACE_DELIMITER = ":"
+
+    # An optional namespace that will prefix all group and permission paths.
+    #
+    attr_accessor :namespace
+
+    # The delimiter that should be used to separate namespaces
+    # in group and permission paths.
+    #
+    # @return [String]
+    def namespace_delimiter
+      @namespace_delimiter ||= DEFAULT_NAMESPACE_DELIMITER
+    end
+    attr_writer :namespace_delimiter
+
+    # Should the namespace be optional when checking permissions?
+    # Defaults to false.
+    #
+    # @return [Boolean]
+    def namespace_optional?
+      @namespace_optional || false
+    end
+    attr_writer :namespace_optional
+
     # The class that should be used to create user proxies.
     #
     # @return [Class]
